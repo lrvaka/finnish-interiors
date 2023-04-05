49df51aa98a956d3ed77a309e36b45d33ec1ed5e
lrvaka <lukevakasisikakala@gmail.com>
2023-04-05 09:55:47 -0400

    fix type error for portfolioItemInfo

--- pages/portfolio/[slug].tsx
+++ pages/portfolio/[slug].tsx
@@ -4,6 +4,38 @@ import Footer from "../../components/ui/Footer";
 import { PortfolioItemHeader } from "../../components/PortfolioComponents/Header";
 import PortfolioItemInfo from "../../components/PortfolioComponents/PortfolioItemInfo";
 import MorePortfolioItems from "../../components/PortfolioComponents/MorePortfolioItems";
+import Head from "../../helpers/Head";
+
+export default function PortfolioItem({
+  project,
+}: {
+  project: typeof projectList[0];
+}) {
+  const meta = {
+    title: `${project.name} - Portfolio - Finnish Interiors`,
+    description: `Check out this project we did - ${project.name} at ${project.location}!`,
+    url: `https://www.finnishinteriors.com/portfolio/${project.slug}`,
+    twitter: "https://twitter.com/finnishinteriors",
+    imageUrl: "https://www.finnishinteriors.com/twitter.png",
+    imageAlt: "Finnish Interiors",
+  };
+
+  return (
+    <>
+      <Head heading={meta} />
+      <main className="pt-[95px] lg:pt-[121px]">
+        <PortfolioItemHeader
+          name={project.name}
+          location={project.location}
+          projectImages={project.projectImages}
+        />
+        <PortfolioItemInfo project={project} />
+        <MorePortfolioItems portfolioItemName={project.name} />
+      </main>
+      <Footer />
+    </>
+  );
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = projectList.map((project) => ({
@@ -18,25 +50,3 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   return { props: { project } };
 };
-
-export default function PortfolioItem({
-  project,
-}: {
-  project: typeof projectList[0];
-}) {
-  return (
-    <>
-      {/* <Head heading={meta} /> */}
-      <main className="pt-[95px] lg:pt-[121px]">
-        <PortfolioItemHeader
-          name={project.name}
-          location={project.location}
-          projectImages={project.projectImages}
-        />
-        <PortfolioItemInfo project={project} />
-        <MorePortfolioItems portfolioItemName={project.name} />
-      </main>
-      <Footer />
-    </>
-  );
-}
