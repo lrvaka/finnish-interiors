14d8d84580ee7ca422469cf47206af1cfc5dedca
lrvaka <lukevakasisikakala@gmail.com>
2023-04-05 02:39:44 -0400

    add portfolio pages

--- components/PortfolioComponents/Header.tsx
+++ components/PortfolioComponents/Header.tsx
@@ -1,8 +1,124 @@
 import { useEffect, useRef, useContext } from "react";
 import Image from "next/image";
 import gsap from "gsap";
-import img from "../../public/images/about/about-us.jpeg";
-import backdrop from "../../public/images/about/backdrop.png";
+
+import { StaticImageData } from "next/image";
+import { ChevronLeftIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/navigation";
+import { Swiper as SwiperType } from "swiper";
+
+const PortfolioItemHeader = ({
+  name,
+  location,
+  projectImages,
+}: {
+  name: string;
+  location: string;
+  projectImages: Array<StaticImageData>;
+}) => {
+  const container = useRef<HTMLDivElement | null>(null);
+  const swiperRef = useRef<SwiperType | null>(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.from("#info-section > *", {
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top bottom", // when the top of the trigger hits the top of the viewport
+        },
+        stagger: 0.1,
+        y: -10,
+        opacity: 0,
+        ease: "power4.easeOut",
+      });
+
+      gsap.from("#inner-img", {
+        scale: 1.1,
+        opacity: 0.5,
+        duration: 5,
+        ease: "power4.easeOut",
+      });
+    }, container); // <- IMPORTANT! Scopes selector text
+
+    return () => {
+      ctx.revert();
+    }; // cleanup
+  }, []); // <- empty dependency Array so it doesn't re-run on every render
+
+  return (
+    <div ref={container}>
+      <div className="relative overflow-hidden">
+        <div className="z-10 relative flex flex-col max-w-screen-xl mx-auto overflow-hidden h-full">
+          <div
+            id="info-section"
+            className="text-center mx-auto mt-20 mb-10 px-4 lg:px-6 max-w-screen-sm  lg:max-w-screen-md relative z-20"
+          >
+            <h1 className="text-black text-3xl lg:text-5xl font-bold">
+              {name}
+            </h1>
+            <p className=" text-slate-500 text-lg leading-normal mt-2 lg:mt-5 lg:text-xl ">
+              {location}
+            </p>
+          </div>
+          <div className="h-[300px] md:h-[600px] lg:h-[900px] relative mb-8">
+            <Swiper
+              id="inner-img"
+              slidesPerView="auto"
+              onBeforeInit={(swiper) => {
+                swiperRef.current = swiper;
+              }}
+              spaceBetween={30}
+              loop={true}
+              breakpoints={{
+                // when window width is >= 320px
+                0: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+              }}
+              className="h-full w-full"
+            >
+              {projectImages.map((e, i) => {
+                return (
+                  <SwiperSlide
+                    className="relative  w-full h-full"
+                    key={"project-item " + i}
+                  >
+                    <Image
+                      className="object-contain"
+                      fill
+                      src={e}
+                      alt={"beautiful picture of " + name}
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+            <div className="absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 justify-between z-10 text-teal-900 flex gap-2 self-center ">
+              <button
+                aria-label="Left button"
+                className="text-3xl p-2 bg-theme-100"
+                onClick={() => swiperRef.current?.slidePrev()}
+              >
+                <ChevronLeftIcon className="text-black h-5 w-5 lg:w-8 lg:h-8" />
+              </button>
+              <button
+                aria-label="Right button"
+                className="text-3xl p-2 bg-theme-100"
+                onClick={() => swiperRef.current?.slideNext()}
+              >
+                <ChevronRightIcon className="text-black h-5 w-5 lg:w-8 lg:h-8" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Header = () => {
   const container = useRef<HTMLDivElement | null>(null);
@@ -50,3 +166,4 @@ const Header = () => {
 };
 
 export default Header;
+export { PortfolioItemHeader };
---
+++ components/PortfolioComponents/MorePortfolioItems.tsx
@@ -0,0 +1,143 @@
+import NextLink from "next/link";
+import Image from "next/image";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+import { projectList } from "./PortfolioItems";
+import { FcFrame } from "react-icons/fc";
+import { IoHammerOutline } from "react-icons/io5";
+import { BiCog } from "react-icons/bi";
+import { MdOutlineCleaningServices } from "react-icons/md";
+
+const MorePortfolioItems = ({
+  portfolioItemName,
+}: {
+  portfolioItemName: string;
+}) => {
+  const container = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.set("#header > *, #projects > *", {
+        y: -10,
+        opacity: 0,
+      });
+
+      gsap.to("#header > *, #projects > *", {
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top bottom", // when the top of the trigger hits the top of the viewport
+        },
+        stagger: 0.2,
+        y: 0,
+        opacity: 1,
+        ease: "power4.easeOut",
+      });
+    }, container); // <- IMPORTANT! Scopes selector text
+
+    return () => {
+      ctx.revert();
+    }; // cleanup
+  }, []); // <- empty dependency Array so it doesn't re-run on every render
+
+  const onEnter = ({ currentTarget }: { currentTarget: any }) => {
+    gsap.context(() => {
+      gsap.to("#inner-img", {
+        scale: 1.05,
+        ease: "power4.easeOut",
+      });
+
+      gsap.to(currentTarget, {
+        y: 10,
+        ease: "power4.easeOut",
+      });
+
+      gsap.to("#heading", {
+        color: "#00406A",
+      });
+    }, currentTarget); // <- IMPORTANT! Scopes selector text
+  };
+
+  const onLeave = ({ currentTarget }: { currentTarget: any }) => {
+    gsap.context(() => {
+      gsap.to("#inner-img", {
+        scale: 1,
+        ease: "power4.easeOut",
+      });
+
+      gsap.to(currentTarget, {
+        y: 0,
+        ease: "power4.easeOut",
+      });
+
+      gsap.to("#heading", {
+        color: "#000000",
+      });
+    }, currentTarget); // <- IMPORTANT! Scopes selector text
+  };
+
+  return (
+    <div ref={container} className="bg-gray-100 ">
+      <div className=" px-4 lg:px-6 py-20 z-10 relative flex flex-col max-w-screen-xl mx-auto overflow-hidden h-full">
+        <div
+          id="header"
+          className=" relative z-20 flex flex-col lg:flex-row justify-between lg:items-end gap-5"
+        >
+          {" "}
+          <h2 className="text-black text-3xl lg:text-4xl font-bold">
+            More of our projects
+          </h2>
+          <NextLink
+            href="/portfolio"
+            className=" max-w-max  border-theme-100 border-2 py-4 px-7 h-fit font-semibold text-theme-200 lg:text-lg transition-all hover:text-theme-200 hover:bg-theme-100 hover:scale-95"
+          >
+            All projects
+          </NextLink>
+        </div>
+        <div
+          id="projects"
+          className="grid grid-cols-1 py-10 gap-10 flex-col lg:grid-cols-2"
+        >
+          {projectList
+            .filter((e, i) => e.name !== portfolioItemName)
+            .slice(0, 2)
+            .map((e, i) => (
+              <NextLink
+                key={e.slug}
+                href={`/portfolio/${e.slug}`}
+                passHref
+                onMouseEnter={onEnter}
+                onMouseLeave={onLeave}
+                className="cursor-pointer relative flex flex-col gap-6 border border-slate-200 border-b-theme-100 border-b-[5px] h-max"
+              >
+                <div
+                  id="main-img"
+                  className="relative w-full  z-10  h-[400px] overflow-hidden"
+                >
+                  <Image
+                    id="inner-img"
+                    className="object-cover"
+                    fill
+                    src={e.img}
+                    alt={"beautiful picture of about us"}
+                  />
+                </div>
+                <div className="p-10 pt-5">
+                  <div className="sm:min-w-0 sm:flex-1">
+                    <h2
+                      id="heading"
+                      className="text-2xl font-semibold leading-8 text-gray-900"
+                    >
+                      {e.name}
+                    </h2>
+                    <p className="text-base  text-slate-500">{e.location}</p>
+                  </div>
+                </div>
+              </NextLink>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MorePortfolioItems;
