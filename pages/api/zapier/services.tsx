351efdcafc14967ec3fd52d9cdc04715d2db01e4
lrvaka <lukevakasisikakala@gmail.com>
2023-07-27 12:06:02 -0400

    add zapier to env

--- pages/api/zapier/services.tsx
+++ pages/api/zapier/services.tsx
@@ -8,7 +8,7 @@ const handler = async (
   if (req.method === "POST") {
     try {
       const response = await axios.post(
-        "https://hooks.zapier.com/hooks/catch***REMOVED***",
+        process.env.NEXT_PUBLIC_ZAPIER_SERVICES!,
         req.body
       );
       res.status(200).json(response.data);
